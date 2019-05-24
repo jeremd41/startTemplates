2370051158a719fe5b812ef423015b0a081d0bab
Jérémy DIARD <44467357+jeremd41@users.noreply.github.com>
2019-05-24 14:39:58 +0200

    supp typograhy.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,11 +64,5 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
   ],
 }
